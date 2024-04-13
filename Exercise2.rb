fca4c4be27e3f6be3723280df5b93796a90af847
vanderleipinto <vandecopinto@gmail.com>
2024-04-13 04:58:02 -0300

    feat: Desafio Lógica de programação - diamantes

---
+++ Exercise2.rb
@@ -0,0 +1,16 @@
+fim = false
+count = 0
+string = "<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>"
+# string.gsub!('.','')
+while !fim 
+  string.sub!('.','')
+  if  string.include?("<>") then
+      string.slice!('<>')
+    count +=1
+    p string
+    else
+      fim = true
+    end  
+end
+
+puts count
